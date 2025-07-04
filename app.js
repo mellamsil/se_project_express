78035bad0dc5a9b40477c1e637d5401cedf71b1d
mellamsil <melvinsil@gmail.com>
2025-07-03 22:54:57 -0400

    Second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
+
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
+
+app.use(express.json());
+app.use("/", mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+  console.log("This is working");
+});
