2c74d81e9eb443c6e0f09fd16330bf37adfe4f45
mellamsil <melvinsil@gmail.com>
2025-08-26 12:55:34 -0400

    Create: a .env file

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,3 @@
-// const JWT_SECRET = "secretkey"; // we will hide the key from other developers.
-
-// module.exports = { JWT_SECRET };
-
-// module.exports = {
-//   JWT_SECRET: process.env.JWT_SECRET || "dev-secret",
-// };
-
 // const JWT_SECRET = process.env.JWT_SECRET || "dev-secret";
 
 const { JWT_SECRET = "dev-secret" } = process.env;
